6313bc86684b986bd71369b3ef15c5575cfa4b5d
JosenildoV <jva@cin.ufpe.br>
2018-07-31 18:10:41 -0300

    Aulas 1, 2 feitas

---
+++ aula2.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //biblioteca de entrada e saida de dados (cout)
+
+using namespace std;
+
+int main(){
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ teste.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "testando \n";
+	return 0;
+}
