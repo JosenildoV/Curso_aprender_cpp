93cc1c5a80de069f8137c499ad4ad54009633c00
Josenildo Vicente de Araujo <jva@ug1c34.windows.cin.ufpe.br>
2018-08-28 14:50:52 -0300

    parte da aula 17 do curso

---
+++ aula17.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+    Ponteiros:
+*/
+void foo(int n){
+    n=30;
+}
+
+void fooPont(int* n){
+    *n = 40;
+}
+
+int main(){
+
+    int var = 10;
+    int* pvar;      //declarando o ponteiro
+    pvar = &var;    /*Inicializando o ponteiro passando
+                      como entrada um endereço de uma variável*/
+    cout << *pvar << endl;//Imprimindo o valor que está no endereço apontado pelo ponteiro
+    *pvar = 20;     //modificando a variável a partir do ponteiro
+    foo(var);
+    cout << var << endl;
+    fooPont(&var);  //Modificando o valor da variável a partir de uma função externa(com ponteiros)
+    cout << var << endl;
+
+    int* vet = new int[10];
+    //*vet[0] = 10;
+    vet[1] = 20;
+    cout << vet << endl;
+    cout << (vet +1) << endl;
+    cout << *(vet + 1) << endl;
+
+    int aux[10];
+    aux[4] = 10;
+    cout << aux[3] << endl;
+
+    return 0;
+}
